b1af2feff7f134795ace442aa47a5c2cdbcffe0c
pioneermushrooms <chris@pioneermushrooms.com>
2025-03-12 03:47:08 -0700

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -41,10 +41,18 @@ export default function Footer() {
             <p className="ml-2 h-7">github</p>
           </a>
         </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.linkedin.com/in/cschuldt/"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">LinkedIn</p>
+          </a>
+        </li>
       </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
-      </p>
     </footer>
   )
 }
